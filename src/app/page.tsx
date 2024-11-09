e60aacb742c1563e877ea5af253ccf997445c06a
Robert Safin <robertsafin98@gmail.com>
2024-11-09 23:41:59 +0800

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-// import Choose from "@/components/home-page/Choose";
+//import Choose from "@/components/home-page/Choose";
 import Experience from "@/components/home-page/Experience";
 import Hero from "@/components/home-page/Hero";
-//import Parallax from "@/components/home-page/Parallax";
+import Parallax from "@/components/home-page/Parallax";
 import Solutions from "@/components/home-page/Solutions";
 import Strip from "@/components/home-page/Strip";
-import Wrapper from "@/components/home-page/Wrapper";
 
 export default async function Home() {
   return (
@@ -12,8 +11,8 @@ export default async function Home() {
       <Hero />
       <Strip />
       <Experience />
-      {/* <Parallax /> */}
-      <Wrapper />
+      <Parallax />
+      {/* <Choose /> */}
       <Solutions />
     </>
   );
